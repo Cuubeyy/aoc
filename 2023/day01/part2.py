4cfec472bc436e72bc3acfe6dd3e8e5f80cef558
CubeDude <zockerkinder@gmx.de>
2023-12-01 07:29:53 +0100

    day01 of 2023 finished

--- 2023/day01/part2.py
+++ 2023/day01/part2.py
@@ -1,31 +1,32 @@
-import os
-from aocd import get_data
-
 task = open("input.txt").read().splitlines()
 ans = 0
 
-numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "eight": 8, "nine": 9, 1: 1, 2: 2, 3: 3,
-           4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
-lines = []
+
+def getFirstNumber(new_line):
+    numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, 1: 1,
+               2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
+    summe = 0
+    for i in range(len(new_line)):
+        temp_line = new_line[i:]
+        for number in numbers:
+            if temp_line.startswith(str(number)):
+                return numbers[number]
+
+
+def getSecondNumber(new_line):
+    numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, 1: 1,
+               2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
+    for i in range(len(new_line), -1, -1):
+        temp_line = new_line[:i]
+        for number in numbers:
+            if temp_line.endswith(str(number)):
+                return numbers[number]
+
 
 for line in task:
-    temp = []
-    for n in numbers:
-        if line.find(str(n)) != -1:
-            y = [i for i in range(len(line)) if line.startswith(str(n), i)]
-            print(y)
-            for yy in y:
-                temp.append((numbers[n], yy))
-
-    if temp:
-        temp.sort(key=lambda x: x[1])
-        print(temp)
-        lines.append(temp)
-
-for line in lines:
-    z = "".join([str(line[0][0]), str(line[-1][0])])
-    ans += int(z)
+    summe = getFirstNumber(line) * 10
+    summe += getSecondNumber(line)
+    print(summe)
+    ans += summe
 print(ans)
 
-# 53502 FALSE
-# 53450 FALSE
