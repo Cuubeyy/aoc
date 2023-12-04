7091eadf2a4e7213ebb50503cc3de9d18742baab
CubeDude <zockerkinder@gmx.de>
2023-12-04 21:36:55 +0100

    Remade most of 22/12

--- 2022/day12/part1.py
+++ 2022/day12/part1.py
@@ -1,75 +1,69 @@
-class Node:
-    def __init__(self, id, position):
-        self.id = id
-        self.position = position
-        self.followers = []
-        self.appendet = []
-
-    def find_way(self, beforeP, path):
-        print(self.id, chr(self.id), self.position)
-        path.append(self.position)
-        for f in self.followers:
-            if f.position != beforeP:
-                return f.find_way(self.position, path)
-            return path
-
-    def __str__(self):
-        return "ID: " + str(self.id) + " position: " + str(self.position) + " followers: " + str(self.followers)
-
-    def add_follower(self, follower):
-        if self.position == (6, 2):
-            pass
-        if follower.game_id == self.id or follower.game_id == self.id + 1:
-            self.followers.append(follower)
-            self.appendet.append(follower.position)
+from collections import defaultdict
 
 
-task = open("test.txt").read().splitlines()
-nodes = []
-grid = []
-for i in range(len(task)):
-    t = task[i]
-    temp = []
-    for k in t:
-        k = ord(k)
-        if k == ord("S"):
-            ax, ay = (t.index("S"), i)
-            k = ord("a")-1
-        elif k == ord("E"):
-            bx, by = (t.index("E"), i)
-            k = ord("z")+1
-        temp.append(k)
-    grid.append(temp)
+def read_task(filename="test.txt"):
+    with open(filename, "r") as file:
+        return file.read().splitlines()
 
-for row_i in range(len(grid)):
-    row = grid[row_i]
-    for position_i in range(len(row)):
-        position = row[position_i]
-        node = Node(position, (position_i, row_i))
-        nodes.append([node, (position_i, row_i)])
 
-for node, position in nodes:
-    for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-        if position[0] + direction[0] >= 0 and position[1] + direction[1] >= 0:
-            next_node = (node.position[0] + direction[0], node.position[1] + direction[1])
-            for n in nodes:
-                if n[1] == next_node:
-                    node.add_follower(n[0])
-                    break
+def build_graph(task):
+    graph = defaultdict(list)
+    start, end = (0, 0), (0, 0)
 
-startnode = None
-for n in nodes:
-    if n[1] == (ax, ay):
-        startnode = n[0]
-        break
-path = startnode.find_way((-1, 0), [])
-print(path, len(path)+1)
+    for y, line in enumerate(task):
+        for x, height in enumerate(line):
+            if height == "S":
+                start = (x, y)
+                height = "a"
+            elif height == "E":
+                end = (x, y)
+                height = "z"
 
-grid2 = []
-for g in grid:
-    g = list(map(chr, g))
-    grid2.append(g)
-for p in path:
-    grid2[p[1]][p[0]] = str(path.index(p))
-for g in grid2:
-    print(g)
+            if y + 1 < len(task) and abs(ord(height) - ord(task[y + 1][x])) <= 1 or (
+                    y + 1 < len(task) and line[y + 1] == "E"):
+                graph[(x, y)].append((x, y + 1))
+            if y - 1 >= 0 and abs(ord(height) - ord(task[y - 1][x])) <= 1 or (y - 1 >= 0 and line[y - 1] == "E"):
+                graph[(x, y)].append((x, y - 1))
+            if x - 1 >= 0 and abs(ord(height) - ord(task[y][x - 1])) <= 1 or (x - 1 >= 0 and line[x - 1] == "E"):
+                graph[(x, y)].append((x - 1, y))
+            if x + 1 < len(line) and abs(ord(height) - ord(line[x + 1])) <= 1 or (
+                    x + 1 < len(line) and line[x + 1] == "E"):
+                graph[(x, y)].append((x + 1, y))
+
+    return graph, start, end
+
+
+def search(graph, start_cord, target_cord):
+    def recursive_search(current_cord, way, step=0):
+        nonlocal min_steps, best_way
+        way.append(current_cord)
+
+        if current_cord == target_cord:
+            if step < min_steps:
+                min_steps = step
+                best_way = way.copy()
+            return
+
+        if step >= len(task[0]) * len(task):
+            return
+
+        connections = graph[current_cord]
+        for c in connections:
+            if c not in walked:
+                walked.add(c)
+                recursive_search(c, way, step + 1)
+                walked.remove(c)
+
+    min_steps = len(task) * len(task[0])
+    best_way = []
+    walked = set()
+
+    recursive_search(start_cord, [])
+
+    return min_steps, best_way
+
+
+task = read_task()
+graph, start, end = build_graph(task)
+result = search(graph, start, end)
+print(result)
