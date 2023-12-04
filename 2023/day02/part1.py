4c27e9f2319e981a31ccdfec413a9ef6f89de4fa
Laptop <zockerkinder@gmx.de>
2023-12-04 08:18:30 +0100

    Changed 23/02

--- 2023/day02/part1.py
+++ 2023/day02/part1.py
@@ -1,16 +1,13 @@
 task = open("input.txt").read().splitlines()
 ans = 0
 
-red_max, green_max, blue_max = 12, 13, 14
-
 for line in task:
-
-    splitted = line.split(": ")
-    id = int(splitted[0].replace("Game ", ""))
+    split = line.split(": ")
+    game_id = int(split[0].replace("Game ", ""))
     valid = True
-    splitted = splitted[1].split(";")
+    split = split[1].split(";")
 
-    for sed in splitted:
+    for sed in split:
         dic = {}
         cubes = sed.split(", ")
 
@@ -18,18 +15,15 @@ for line in task:
             cube = cube.split()
             dic[cube[1]] = int(cube[0])
 
-        for k in dic.keys():
-            if k == "blue":
-                if dic["blue"] > 14:
-                    valid = False
-            if k == "green":
-                if dic["green"] > 13:
-                    valid = False
-            if k == "red":
-                if dic["red"] > 12:
-                    valid = False
+        for k in dic:
+            if k == "blue" and dic[k] > 14:
+                valid = False
+            if k == "green" and dic[k] > 13:
+                valid = False
+            if k == "red" and dic[k] > 12:
+                valid = False
 
     print(valid)
-    ans += id if valid == True else 0
+    ans += game_id if valid else 0
 
 print(ans)
