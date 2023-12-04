ea5f72814fedd8555d3a59ef41cb73495d8bde41
CubeDude <zockerkinder@gmx.de>
2023-12-04 06:31:40 +0100

    day04 of 2023 finished

---
+++ 2023/day04/part1.py
@@ -0,0 +1,19 @@
+task = open("input.txt").read().splitlines()
+ans = 0
+
+for line in task:
+    temp_win = 0
+    winning, numbers = line.split(": ")[1].split(" | ")
+
+    winning = winning.split()
+    numbers = numbers.split()
+
+    for n in numbers:
+        if n in winning:
+            if temp_win == 0:
+                temp_win = 1
+            else:
+                temp_win *= 2
+    print(temp_win)
+    ans += temp_win
+print(ans)
---
+++ 2023/day04/part2.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+task = open("input.txt").read().splitlines()
+ans = 0
+
+cards = defaultdict(lambda: 1)
+
+for card_number, line in enumerate(task):
+    temp_win = 0
+    winning, numbers = line.split(": ")[1].split(" | ")
+
+    winning = winning.split()
+    numbers = numbers.split()
+    for n in numbers:
+        if n in winning:
+            temp_win += 1
+
+    for l in range(cards[str(card_number)]):
+        for i in range(temp_win):
+            cards[str(card_number + 1 + i)] += 1
+
+print(cards)
+for c in cards:
+    ans += cards[c]
+print(ans)
+
+# 1070 FALSE
