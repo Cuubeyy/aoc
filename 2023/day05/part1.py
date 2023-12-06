6df9067573b8c3eb88a3e8607eb8267a37fed230
CubeDude <zockerkinder@gmx.de>
2023-12-06 06:20:25 +0100

    CLEANED 23/05

--- 2023/day05/part1.py
+++ 2023/day05/part1.py
@@ -1,13 +1,10 @@
 from datetime import datetime
-
 start = datetime.now()
 task = open("input.txt").read().splitlines()
 ans = 10e10
-
 seeds = list(map(int, task.pop(0).split(": ")[1].split()))
 task.pop(0)
 temp_seeds = [seed for seed in seeds]
-
 for line_index, line in enumerate(task):
     possible = []
     if line == "":
@@ -19,7 +16,6 @@ for line_index, line in enumerate(task):
     for seed_index, seed in enumerate(seeds):
         if int(range_start) <= seed <= int(range_start) + int(source_range):
             temp_seeds[seed_index] = int(destination) + (seed - int(range_start))
-
 seeds = temp_seeds
 print(min(seeds))
 print(datetime.now() - start)
