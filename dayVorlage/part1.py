59fbe63b585aa2e61bbe94ca8d36231818baacbf
Cuubeyy <62384517+Cuubeyy@users.noreply.github.com>
2023-12-07 17:10:37 +0100

    Update part1.py

--- dayVorlage/part1.py
+++ dayVorlage/part1.py
@@ -2,7 +2,7 @@ import os
 from aocd import get_data
 
 if os.stat("input.txt").st_size == 0:
-    session = "53616c7465645f5f0fee3a4adba2d4850f273dec46cc7a411debbc92dcd73989d552fb397854b3819375fe33e3221345dfd1f70c3b4357549a127747ba19f5e3"
+    session = ""
     data = get_data(session)
     with open("input.txt", "w") as f:
         f.write(data)
