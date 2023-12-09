d1d76426c8120e634cbf336c93d7d70cfa611596
CubeDude <zockerkinder@gmx.de>
2023-12-09 06:38:28 +0100

    FINISHED 23/09

---
+++ 2023/day09/part1.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import os
+from aocd import get_data
+import math
+
+start = datetime.now()
+task = open("input.txt").read().splitlines()
+ans = 0
+
+
+def add_layer(layers_):
+    temp = []
+    for i, number in enumerate(layers[-1][:-1]):
+        temp.append(layers[-1][i + 1] - number)
+    layers_.append(temp)
+    return layers_
+
+
+for line in task:
+    layers = []
+    numbers = list(map(int, line.split()))
+    layers.append(numbers)
+    while not all(v == 0 for v in layers[-1]):
+        layers = add_layer(layers)
+
+    last = 0
+    for i, layer in enumerate(reversed(layers)):
+        if i == 0:
+            layer.insert(0, 0)
+            continue
+        last = layer[0] - last
+        layer.insert(0, last)
+    ans += last
+    print(layers)
+
+print(ans)
+print(datetime.now() - start)
+
+# 1973284947 FALSE
