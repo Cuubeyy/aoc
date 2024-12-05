e34054f78419c036c593c5430bfb4142d6c7182e
Emil Dahlke <emil@konfidal.eu>
2024-12-05 09:43:34 +0100

    template formatting

---
+++ solutionT.py
@@ -0,0 +1,24 @@
+from aoctools.TextGrid import TextGrid
+from aoctools.Input import Input
+import re
+from collections import defaultdict
+import sys
+
+sys.setrecursionlimit(1000000)
+
+ans = 0
+
+# line by line input
+task = Input().get_input()
+for line in task:
+    pass
+
+# one line input
+task = Input().get_input()
+for ch in task:
+    pass
+
+# grid_input
+task = Input().get_input()
+grid = TextGrid(task)
+print(grid)
