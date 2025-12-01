0e155a356c57cce6ef336ab72cf64df7c216c0c4
Emil Dahlke <emil@konfidal.eu>
2025-12-01 06:25:25 +0100

    template

--- solutionT.py
+++ solutionT.py
@@ -6,16 +6,17 @@ import sys
 
 sys.setrecursionlimit(1000000)
 
+directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 ans = 0
 
 # line by line input
-task = Input().get_input()
+task = Input().get_input().splitlines()
 for line in task:
     pass
 
 # one line input
 task = Input().get_input()
-for ch in task:
+for char in task:
     pass
 
 # grid_input
